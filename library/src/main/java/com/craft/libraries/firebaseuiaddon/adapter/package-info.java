a4a87a3fcc22e8cc33e3e6af106049bfc32600c3
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-25 23:40:39 -0600

    Add package-info files to library

---
+++ library/src/main/java/com/craft/libraries/firebaseuiaddon/adapter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A set of adapters to bind UI to Firebase Realtime Database or Firestore locations.
+ *
+ * @see com.craft.libraries.firebaseuiaddon.FirebaseSpinnerAdapter
+ * @see com.craft.libraries.firebaseuiaddon.FirestoreSpinnerAdapter
+ */
+package com.craft.libraries.firebaseuiaddon.adapter;
---
+++ library/src/main/java/com/craft/libraries/firebaseuiaddon/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * An additional set of Firebase UI bindings for Android.
+ *
+ * @version 2.0.0
+ */
+package com.craft.libraries.firebaseuiaddon;
