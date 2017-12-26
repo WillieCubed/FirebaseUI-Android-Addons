db6cee407e371e8c05fb435cb5ddedc0e1c9604f
Willie Chalmers III <willieechalmers@gmail.com>
2017-12-25 23:20:22 -0600

    Update FirebaseSpinnerAdapter
     - Bump to FirebaseUI 3.0.0
     - Update docs
     - Explicitly define populateView

--- library/src/main/java/com/craft/libraries/firebaseuiaddon/FirebaseSpinnerAdapter.java
+++ library/src/main/java/com/craft/libraries/firebaseuiaddon/FirebaseSpinnerAdapter.java
@@ -1,111 +1,80 @@
 package com.craft.libraries.firebaseuiaddon;
 
-import android.app.Activity;
-import android.util.Log;
+import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SpinnerAdapter;
+
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Query;
+import com.firebase.ui.database.FirebaseListOptions;
 
 /**
  * This class is a generic way of backing a Spinner with a Firebase location. It handles all of the
  * child events at the given Firebase location. It marshals received data into the given class type.
  * <p> Subclasses are responsible for populating an instance of the corresponding view with the data
- * from an instance of modelClass by providing an implementation of {@code populateView} and
- * {@code populateDropdownView}, which will be given an instance of your spinner item, your
- * dropdown list item, and an instance your class that holds your data. Populate the views
- * and this class will handle updating the list as the data changes at the given Firebase Database
- * location.
+ * from an instance of modelClass by providing an implementation of {@code populateView} and {@code
+ * populateDropdownView}, which will be given an instance of your spinner item, your dropdown list
+ * item, and an instance your class that holds your data. Populate the views and this class will
+ * handle updating the list as the data changes at the given Firebase Database location.
  *
  * <blockquote><pre>
  * {@code
  *      public class MyActivity extends AppCompatActivity {
- *          @Override
+ *          {@literal @}Override
  *          public void onCreate(Bundle savedInstanceState) {
- *              // Create a Spinner in your XML file and reference it in your Activity code
  *              Spinner spinner = (Spinner) findViewById(R.id.spinner);
- *
- *              // Create a DatabaseReference to the data you wish to populate your Spinner with
- *              DatabaseReference colorReference = FirebaseDatabase.getInstance().getReference();
- *              spinner.setAdapter(new FirebaseSpinnerAdapter<FavoriteColor>(this,
- *                      FavoriteColor.class,android.R.layout.simple_spinner_item,
- *                      android.R.layout.simple_spinner_dropdown_item, colorReference) {
- *
- *                  // Override populateView() to modify the default (unselected) Spinner view
- *                  // displayed with the data from your model
- *                  @Override
- *                  protected void populateView(View view, FavoriteColor color, int position) {
- *                      TextView color = view.findViewById(android.R.id.text1);
- *                      color.setText(color.getName());
+ *              DatabaseReference colorReference = FirebaseDatabase.getInstance().getReference()
+ *                      .child("colors");
+ *              FirebaseListOptions options = new FirebaseListOptions.Builder()
+ *                      .setQuery(colorReference)
+ *                      .setLayout(android.R.layout.simple_spinner_item)
+ *                      .build();
+ *              spinner.setAdapter(new FirebaseSpinnerAdapter<FavoriteColor>(options,
+ *                      android.R.layout.simple_spinner_dropdown_item) {
+ *                 {@literal @}Override
+ * `                protected void populateView(View view, FavoriteColor color, int position) {
+ *                      ((TextView) view.findViewById(android.R.id.text1)).setText(color.getName());
  *                  }
  *
- *                  // Override populateDropdownView() to modify the Spinner's dropdown view
- *                  // displayed with the data from your model
- *                  @Override
+ *                {@literal @}Override
  *                  protected void populateDropdownView(View dropdownView, FavoriteColor color, int
- *                          position) {
- *                      TextView colorDropdown = dropdownView.findViewById(android.R.id.text1);
- *                      colorDropdown.setText(color.getName());
- *                  }
- *              });
- *          }
- *      }
+ *                                                      position) {
+ *                      ((TextView) dropdownView.findViewById(android.R.id.text1)).setText(color.getName());
+ *                 }
+ *             });
+ *         }
+ *     }
  * }
  * </pre></blockquote>
  *
  * @param <T> The class type to use as a model for the data contained in the children of the given
  * Firebase location
+ * @version 2.0.0
+ * @since 1.0.0
  */
 public abstract class FirebaseSpinnerAdapter<T> extends FirebaseListAdapter<T> implements
         SpinnerAdapter {
 
-    private static final String TAG = "FirebaseSpinnerAdapter";
-
-    private int mDropDownLayout;
-
-    /**
-     * @param activity A context used to inflate the Spinner views
-     * @param modelClass A class which Firebase will marshall the data into
-     * @param layout A layout used to represent a single list item.
-     * @param dropdownLayout A layout used to represent a single list item when the spinner is in
-     * the selection state (dropdown/dialog)
-     * @param query The Firebase location to watch for data changes. Can also be a slice of a
-     * location, using some combination of {@code limit()}, {@code startAt()}, and {@code endAt()},
-     */
-    public FirebaseSpinnerAdapter(Activity activity, Class<T> modelClass, int layout,
-            int dropdownLayout, Query query) {
-        super(activity, modelClass, layout, query);
-        mDropDownLayout = dropdownLayout;
-    }
+    @LayoutRes
+    private int mDropdownLayout;
 
     /**
-     * @param activity A context used to inflate the Spinner views
-     * @param modelClass A class which Firebase will marshall the data into
-     * @param layout A layout used to represent a single list item.
-     * @param dropdownLayout A layout used to represent a single list item when the spinner is in
-     * the selection state (dropdown/dialog)
-     * @param reference The Firebase location to watch for data changes. Can also be a slice of a
-     * location, using some combination of {@code limit()}, {@code startAt()}, and {@code endAt()},
+     * Creates a new FirebaseSpinnerAdapter with the given options and dropdown layout.
+     *
+     * @param dropdownLayout The layout to inflate for the spinner dropdown menu
      */
-    public FirebaseSpinnerAdapter(Activity activity, Class<T> modelClass, int layout,
-            int dropdownLayout, DatabaseReference reference) {
-        super(activity, modelClass, layout, reference);
-        mDropDownLayout = dropdownLayout;
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-        Log.w(TAG, databaseError.toException());
+    @SuppressWarnings("WeakerAccess")
+    public FirebaseSpinnerAdapter(FirebaseListOptions<T> options, @LayoutRes int dropdownLayout) {
+        super(options);
+        mDropdownLayout = dropdownLayout;
     }
 
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(mDropDownLayout, parent, false);
+            convertView = LayoutInflater.from(parent.getContext())
+                    .inflate(mDropdownLayout, parent, false);
         }
         T model = getItem(position);
         populateDropdownView(convertView, model, position);
@@ -114,15 +83,31 @@ public abstract class FirebaseSpinnerAdapter<T> extends FirebaseListAdapter<T> i
 
     /**
      * Each time the data at the given Firebase location changes, this method will be called for
-     * each item that needs to be displayed. The first two arguments correspond to the {@code
-     * dropdownLayout} and {@code modelClass} given to the constructor of this class. The third
-     * argument is the item's position in the list which can be used to obtain the item using {@link
-     * #getItem(int)} <p> Subclass implementation should populate the dropdown view using the data
-     * contained in the model.
+     * each item that needs to be displayed. The first two arguments correspond to the layout and
+     * model class given to the {@link FirebaseListOptions} passed into the constructor. The third
+     * argument is the item's position in the list.
+     * <p>
+     * Your implementation should populate the view using the data contained in the model.
+     *
+     * @param view The view to populate
+     * @param model The object containing the data used to populate the view
+     * @param position The position in the list of the view being populated
+     */
+    @Override
+    protected abstract void populateView(View view, T model, int position);
+
+    /**
+     * Each time the data at the given Firebase location changes, this method will be called for
+     * each item that needs to be displayed. The first two arguments correspond to the layout and
+     * model class given to the {@link FirebaseListOptions} passed into the constructor. The third
+     * argument is the item's position in the list.
+     * <p>
+     * Your implementation should populate the view using the data contained in the model.
      *
      * @param dropdownView The spinner's dropdown view to populate
      * @param model The object containing the data used to populate the view
      * @param position The position in the list of the view being populated
      */
-    abstract protected void populateDropdownView(View dropdownView, T model, int position);
+    @SuppressWarnings("WeakerAccess")
+    protected abstract void populateDropdownView(View dropdownView, T model, int position);
 }
